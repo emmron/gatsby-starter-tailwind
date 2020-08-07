3bf6161af7ae160bd07af3053abb88efc0c29a86
Emmett Hoolahan <emmetthoolahan@gmail.com>
2020-08-07 22:06:26 +0800

    aa
    aa

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 import kebabLogo from "../images/logo.jpg";
 
 function IndexPage() {
