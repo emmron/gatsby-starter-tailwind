98c1691f093a57b2e6d467512c2933a316209a7b
Emmett Hoolahan <emmetthoolahan@gmail.com>
2020-08-07 22:05:54 +0800

    eee
    ee

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import kebabLogo from "../images/logo.jpg";
 
 function IndexPage() {
   return (
@@ -16,7 +17,7 @@ function IndexPage() {
         <img
           alt="Cat and human sitting on a couch"
           className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
+          src={kebabLogo}
         />
 
         <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
